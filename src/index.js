ff430fe8cb6f3534ec17476a7ffd50512dc2d260
Andrew Shaffer <darawk@gmail.com>
2015-10-20 02:40:25 -0700

    use strings

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@
  * Imports
  */
 
-import {actions} from 'virtex'
 import setAttribute from './setAttribute'
 import removeAttribute from './removeAttribute'
 import createElement from './createElement'
-
-/**
- * Vars
- */
-
-const {types} = actions
 
 /**
  * Virtex DOM effects driver
@@ -25,21 +18,21 @@ function handle (doc, dispatch, next, action) {
   const {type, payload} = action
 
   switch (type) {
-    case types.CREATE_TEXT_NODE:
+    case 'CREATE_TEXT_NODE':
       return doc.createTextNode(payload)
-    case types.CREATE_ELEMENT:
+    case 'CREATE_ELEMENT':
       return createElement(doc, dispatch, payload.tag, payload.attrs, payload.children)
-    case types.SET_ATTRIBUTE:
+    case 'SET_ATTRIBUTE':
       return setAttribute(dispatch, payload.node, payload.name, payload.value)
-    case types.REMOVE_ATTRIBUTE:
+    case 'REMOVE_ATTRIBUTE':
       return removeAttribute(payload.node, payload.name, payload.priorValue)
-    case types.APPEND_CHILD:
+    case 'APPEND_CHILD':
       return payload.node.appendChild(payload.childNode)
-    case types.REPLACE_CHILD:
+    case 'REPLACE_CHILD':
       return payload.node.replaceChild(payload.newChild, payload.oldChild)
-    case types.INSERT_BEFORE:
+    case 'INSERT_BEFORE':
       return payload.node.insertBefore(payload.newChild, payload.oldChild)
-    case types.REMOVE_CHILD:
+    case 'REMOVE_CHILD':
       return payload.node.removeChild(payload.childNode)
     default:
       return next(action)

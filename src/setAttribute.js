678891315492dfcf61bccfe304ecc35b5566d7ad
Andrew Shaffer <andrew@weo.io>
2016-05-02 15:06:16 -0700

    re-revert, turns out they do need to be reflected

--- src/setAttribute.js
+++ src/setAttribute.js
@@ -27,6 +27,8 @@ function setAttribute (node, name, value, prevValue) {
       case 'selected':
       case 'innerHTML':
       case 'textContent':
+      case 'defaultValue':
+      case 'defaultChecked':
         node[name] = value
         break
       case 'value':
